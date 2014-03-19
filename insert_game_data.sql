ab13ed14231cb4101425e4d767aad88491bbeedd
slavik81 <slavik81@gmail.com>
2014-03-19 16:45:13 -0600

    Adding test and game data.

---
+++ insert_game_data.sql
@@ -0,0 +1,1 @@
+insert into item_type (Name, IconPath, Rarity, Description) values ('Fountain of Money', 'fountain_of_money.png', 2, 'Receive 1000 credits per day.');
---
+++ insert_test_data.sql
@@ -0,0 +1,7 @@
+# note: enum AccountType { USER = 0; ADMIN = 1 }
+
+# new user with password:
+# veryweakpassword
+# bcrypt 8 rounds
+insert into user (Email, Nickname, AccountType) values ('fred@example.com', 'freddie', 0);
+insert into credential (SaltedHash, User, Active) values ('$2a$08$ch19z6jPgjPuopXkpL1j6eM6kjMxx17p/QQz/D/VwAIlMQhrPdqiC', 1, true);
