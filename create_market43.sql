9f141fdd56c65a7dba1cc8231492ed3b1b10c39b
slavik81 <slavik81@gmail.com>
2014-03-19 16:03:04 -0600

    Switching to int id on ItemType for performance reasons.

--- create_market43.sql
+++ create_market43.sql
@@ -1,9 +1,9 @@
 create database market43 character set = utf8mb4 collate = utf8mb4_unicode_ci;
 use market43;
 create table user (UserId int unsigned auto_increment, Email varbinary(254) unique not null, Nickname varchar(20) not null, AccountType tinyint not null, primary key (UserId));
-create table item_type (Name varchar(32) not null, IconPath varbinary(255) not null, rarity tinyint not null, description text, primary key (Name));
-create table item (ItemId int unsigned auto_increment, ItemTypeName varchar(32), OwnerUserId int unsigned, primary key (ItemId), foreign key (ItemTypeName) references item_type(Name), foreign key (OwnerUserId) references user(UserId));
-create table recipe (InputItemType varchar(32), InputItemCount tinyint, OutputItemType varchar(32), foreign key (InputItemType) references item_type(Name), primary key (InputItemType, InputItemCount, OutputItemType), foreign key (OutputItemType) references item_type(Name));
+create table item_type (ItemTypeId int unsigned auto_increment, Name varchar(32) not null, IconPath varbinary(255) not null, rarity tinyint not null, description text, primary key (ItemTypeId));
+create table item (ItemId int unsigned auto_increment, ItemType int unsigned, OwnerUserId int unsigned, primary key (ItemId), foreign key (ItemType) references item_type(ItemTypeId), foreign key (OwnerUserId) references user(UserId));
+create table recipe (InputItemType int unsigned, InputItemCount tinyint, OutputItemType int unsigned, foreign key (InputItemType) references item_type(ItemTypeId), primary key (InputItemType, InputItemCount, OutputItemType), foreign key (OutputItemType) references item_type(ItemTypeId));
 create table listing (ListingId int unsigned auto_increment, MinimumBid int unsigned, PostedTimestamp timestamp, ExpiryTimestamp timestamp, ListedItemId int unsigned, ListingUserId int unsigned, primary key (ListingId), foreign key (ListedItemId) references item(ItemId), foreign key (ListingUserId) references user(UserId));
 create table transaction (TransactionId int unsigned, NetBalanceChange int, TransactionType tinyint, TransactionUser int unsigned, primary key (TransactionId), foreign key (TransactionUser) references user(UserId));
 create table bid (Bidder int unsigned, Listing int unsigned, Value int unsigned, primary key (Bidder, Listing, Value), foreign key (Bidder) references user(UserId), foreign key (Listing) references listing(ListingId));
