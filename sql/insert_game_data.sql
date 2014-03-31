0a192e8327b75c7d5b0237351db88935d6cc0ce6
slavik81 <slavik81@gmail.com>
2014-03-31 00:45:08 -0600

    Adding sane line breaks.

--- sql/insert_game_data.sql
+++ sql/insert_game_data.sql
@@ -1,1 +1,5 @@
-insert into item_type (Name, IconPath, Rarity, Description) values ('Fountain of Money', 'fountain_of_money.png', 2, 'Receive 1000 credits per day.');
+insert into item_type (Name, IconPath, Rarity, Description) values (
+  'Fountain of Money',
+  'fountain_of_money.png',
+  2,
+  'Receive 1000 credits per day.');
--- sql/insert_test_data.sql
+++ sql/insert_test_data.sql
@@ -3,5 +3,11 @@
 # new user with password:
 # veryweakpassword
 # bcrypt 8 rounds
-insert into user (Email, Nickname, AccountType) values ('fred@example.com', 'freddie', 0);
-insert into credential (SaltedHash, User, Active) values ('$2a$08$ch19z6jPgjPuopXkpL1j6eM6kjMxx17p/QQz/D/VwAIlMQhrPdqiC', 1, true);
+insert into user (Email, Nickname, AccountType) values (
+  'fred@example.com',
+  'freddie',
+  0);
+insert into credential (SaltedHash, User, Active) values (
+  '$2a$08$ch19z6jPgjPuopXkpL1j6eM6kjMxx17p/QQz/D/VwAIlMQhrPdqiC',
+  1,
+  true);
