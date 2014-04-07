dc7501059533da65b5dff59af4b1355a3d8fd106
slavik81 <slavik81@gmail.com>
2014-04-07 01:36:05 -0600

    Added count to inventory page.

--- html/inventory.php
+++ html/inventory.php
@@ -32,22 +32,26 @@
 	session_start() or die('Failed to create session');
 	$userid=$_SESSION['userid'];
 	!empty($userid) or die('Session lacks userid!');
-	$query="SELECT DISTINCT(i.ItemId), t.Name, t.IconPath, t.Rarity, t.Description FROM item AS i INNER JOIN item_type AS t ON i.ItemType = t.ItemTypeId INNER JOIN user ON i.OwnerUserId = '$userid';";
+	$query="SELECT COUNT(DISTINCT(i.ItemId)) Count, t.Name, t.IconPath, t.Rarity, t.Description
+		FROM item AS i
+		INNER JOIN item_type AS t ON i.ItemType = t.ItemTypeId
+		INNER JOIN user ON i.OwnerUserId = '$userid';";
+
 	$result=mysql_query($query) or die (mysql_error());
 	$num=mysql_numrows($result);
 	$i=0; while ($i < $num) { 
-	$name=mysql_result($result, $i, "t.Name");
-	$icon=mysql_result($result, $i, "t.IconPath");
-	$description=mysql_result($result, $i, "t.Description");
-	echo "<tr>
-		<td></td>
-		<td><img class=\"item-icon\" src=\"$icon\">$name</td>
-		<td>$description</td>
-		<td></td>
-	</tr>";
-	$i++;}
-
-	mysql_close();
+		$count=mysql_result($result, $i, "Count");
+		$name=mysql_result($result, $i, "t.Name");
+		$icon=mysql_result($result, $i, "t.IconPath");
+		$description=mysql_result($result, $i, "t.Description");
+		echo "<tr>
+			<td>$count</td>
+			<td><img class=\"item-icon\" src=\"$icon\">$name</td>
+			<td>$description</td>
+			<td></td>
+		</tr>";
+		$i++;
+	}
  ?>
 
 </table>
