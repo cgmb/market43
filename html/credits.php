a89c41d80403d9ba5f4801551047b9156eef6145
slavik81 <slavik81@gmail.com>
2014-04-07 15:47:58 -0600

    Updated credits

--- html/credits.php
+++ html/credits.php
@@ -13,10 +13,13 @@
 ?>
 
 <H1>Credits</H1>
+<em>Alex Dang</em>, 
+<em>Cory Bloor</em>, 
+<em>Kathy Le</em>, 
+<em>Keith Kinder</em>
 
 <H2>Creative Commons Art</H2>
-"<a href="fountain_of_money.png">Well-0</a>" by <a href="http://opengameart.org/users/varkalandar">Varkalandar</a> is licensed under <a href="http://creativecommons.org/licenses/by-sa/3.0/">CC-BY_SA 3.0</a>
-
+"<a href="fountain_of_money.png">Well</a>" by <a href="http://opengameart.org/users/varkalandar">Varkalandar</a> is licensed under <a href="http://creativecommons.org/licenses/by-sa/3.0/">CC-BY_SA 3.0</a>
 
 </body>
 </html>
