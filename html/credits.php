42813f92523e8f60422b89b52b67d4cb09cc46a4
slavik81 <slavik81@gmail.com>
2014-04-07 22:44:53 -0600

    Updated art credits.

--- html/credits.php
+++ html/credits.php
@@ -19,8 +19,8 @@
 <em>Keith Kinder</em>
 
 <H2>Creative Commons Art</H2>
-"<a href="fountain_of_money.png">Well</a>" by <a href="http://opengameart.org/users/varkalandar">Varkalandar</a> is licensed under <a href="http://creativecommons.org/licenses/by-sa/3.0/">CC-BY_SA 3.0</a>
+"<a href="fountain_of_money.png">Well</a>", "<a href="fountain_of_money-2.png">Well With Roof</a>" by <a href="http://opengameart.org/users/varkalandar">Varkalandar</a> licensed under <a href="http://creativecommons.org/licenses/by-sa/3.0/">CC-BY_SA 3.0</a>
 <br>
-"<a href="leaf-1.png">cure-1</a>" by <a href="http://opengameart.org/users/eleazzaar">J. W. Bjerk (eleazzaar)</a> is licensed under <a href="http://creativecommons.org/licenses/by-sa/3.0/">CC-BY_SA 3.0</a>
+"<a href="leaf-1.png">cure-1</a>", "<a href="leaf-2.png">cure-2</a>", "<a href="leaf-3.png">cure-3</a>" by <a href="http://opengameart.org/users/eleazzaar">J. W. Bjerk (eleazzaar)</a> licensed under <a href="http://creativecommons.org/licenses/by-sa/3.0/">CC-BY_SA 3.0</a>
 </body>
 </html>
